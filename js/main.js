66f39f9feb4bb9444f0cc65d035e8bda74bd8c82
Vadim <vadim.dubinin001@mail.ru>
2021-07-05 01:26:35 +0400

    add adaptive v.1.0

--- js/main.js
+++ js/main.js
@@ -11,4 +11,30 @@ $(function(){
             }
         ]
     });
+
+    let $window = $(window);
+    
+    function resizeEm(){
+        let currWidth = $window.width();
+        let WidthWindowNow = 761; // Задаем ширину экрана, после которого начнётся скрипт
+        let put = (currWidth / 2) - WidthWindowNow;
+        $('#welcome').animate({left: put}, 1000);
+    }
+
+    // $window.resize(function() {
+    //     $('.gallery-cat__container-images--slider').slick({
+    //         nextArrow: '<button class="slick-arrow slick-next"><img src="images/next-arrow__img.svg" alt="next arrow"></button>',
+    //         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev-arrow__img.svg" alt="prev arrow"></button>',
+    //         responsive: [
+    //             {
+    //                 breakpoint: 320,
+    //                 settings: {
+    //                     arrows: false
+    //                 }
+    //             }
+    //         ]
+    //     });
+    //     resizeEm();
+        
+    // });
 });
